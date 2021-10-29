e8c41e0c001052ced47999771f107d552ab4ec79
Ketan Jitendra Shinde <ks27543926@gmail.com>
2021-10-29 19:52:13 +0530

    Implemented DoubleArray
    Implemented DoubleArray
    
    Signed-off-by: Ketan Jitendra Shinde <ks27543926@gmail.com>

--- src/in/pelligent/Main.java
+++ src/in/pelligent/Main.java
@@ -15,5 +15,22 @@ public class Main {
         System.out.println(array);
         System.out.printf("find(21): %d\n", array.find(21));
         System.out.printf("contains(5): %b\n", array.contains(5));
+
+        System.out.println("******************************************************");
+
+        DoubleArray doubleArray = new DoubleArray(6);
+        doubleArray.add(10.25);
+        doubleArray.add(8.21);
+        doubleArray.add(2.50);
+        doubleArray.add(25.00);
+        System.out.printf("double array: %s\n", doubleArray);
+        doubleArray.reverse();
+        System.out.printf("double array reversed: %s\n", doubleArray);
+        doubleArray.sort(true);
+        System.out.printf("double array sorted: %s\n", doubleArray);
+        System.out.println(doubleArray);
+        System.out.printf("find(2.50): %d\n", doubleArray.find(2.50));
+        System.out.printf("contains(8.21): %b\n", doubleArray.contains(8.21));
+
     }
 }
